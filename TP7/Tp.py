01f08f6e9f8a2e24badb32068c1f695b5894fca4
smx2020 <olivier@doussaud.org>
2017-01-11 23:33:36 +0100

    Exercice A-1 fait entierement

---
+++ TP7/Tp.py
@@ -0,0 +1,23 @@
+def	check_matrice(M):	#M matrice
+	"verifie que la matrice soit rectangulaire"
+	comp = len(M[0])
+	for i in range(len(M)):
+		if len(M[i]) != comp:
+			return(True)
+	return(False)
+
+def	trace_matrice(M):		#M matrice
+	"Calcule la somme des coefficient de la diagonal"
+	if check_matrice(M):
+		return("Erreur")
+	out = 0				#Somme des coeff
+	for i in range(len(M)):
+		out = out + M[i][i]
+	return(out)
+
+def	matrice_identite(n):				#n entier
+	out = [[0 for j in range(n)]for i in range(n)]	#cree la matrice vide
+	for i in range(n):
+		out[i][i] = 1
+	return(out)
+
--- TP7/test_A1.py
+++ TP7/test_A1.py
@@ -1,16 +1,18 @@
+from Tp import *
+
 #	Exo A-1-1
 
-A = [[1,2,3],
-	[4,0,-1]]
+A = [[1,2,3],[4,0,-1]]
 
 print(len(A),len(A[0]))		#nb de ligne et nb de colonne
-print(A[1,0])				#a(2,1)
-print(A[1])					#Deuxieme ligne de A
-print(A[0][1],A[1][1])		#Deuxieme colonne de A
+print(A[1][0])			#a(2,1)
+print(A[1])			#Deuxieme ligne de A
+print([A[0][1],A[1][1]])	#Deuxieme colonne de A
 print(A[0][2] + A[1][2])	#Somme des coefficient de la 3eme colonne
 
 #	Exo A-1-2
 
-A =[[1,2,3],
-	[4,5,6],
-	[7,8,9] ]
+A =[[1,2,3],[4,5,6],[7,8,9] ]
+
+print(trace_matrice(A))
+print(matrice_identite(3))
