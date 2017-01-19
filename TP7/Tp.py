94abe7794af066a5d8dc45982023e5420f424f20
smx2020 <olivier@doussaud.org>
2017-01-19 14:04:21 +0100

    Ajout

--- TP7/Tp.py
+++ TP7/Tp.py
@@ -53,6 +53,37 @@ def	multiplier_ligne_c(A,I,l):
 		temp = []
 		for j in range(len(A[0])):
 			temp.append(A[i][j])
-		out.append(temp)
-	return(multiplier_ligne(A,I,l))
+			out.append(temp)
+			return(multiplier_ligne(A,I,l))
 
+def addition_ligne(A,i1,i2):
+	"Additionne la ligne i1 et i2"
+	for j in range(A[0]):
+		A[i1][j] = A[i1][j] + A[i2][j]
+	return(A)
+
+def addition_ligne_c(A,i1,i2):
+	"Cree une nouvelle matrice et fais addition_ligne"
+	out = []
+	for i in range(len(A)):
+		temp = []
+		for j in range(len(A[0])):
+			temp.append(A[i][j])
+		out.append(temp)
+	return(addition_ligne(out,i1,i2))
+
+def addition_multiple_ligne(A,i1,i2,l):
+	"Additionne et multiplie la ligne i2 a i2"
+	for j in range(A[0]):
+		A[i1][j] = A[i1][j] + l * A[i2][j]
+	return(A)
+
+def addition_multiple_ligne_c(A,i1,i2,l):
+	"Cree une nouvelle matrice et fais addition_ligne"
+	out = []
+	for i in range(len(A)):
+		temp = []
+		for j in range(len(A[0])):
+			temp.append(A[i][j])
+		out.append(temp)
+	return(addition_multiple_ligne(out,i1,i2,l))
