5abd6f5eb7bc8fc7fe9eb41e41bc41db48c26897
smx2020 <smx2020@gmail.com>
2017-03-16 14:05:51 +0100

    test

--- TP09/fonction.py
+++ TP09/fonction.py
@@ -5,7 +5,9 @@ def Euler1(tmin,tmax,F,y0,n):
 	h = (tmax-tmin)/n
 	y = [y0]
 	t = [tmin]
-	for k in range(1,n):
+	for k in range(n):
 		y.append(y[k] + h*F(y[k],t[k]))
 		t.append(tmin + h*k)
-	return(y,t)
+	return([y,t])
+
+def trace_Euler1(tmin,tmax,F,y0,n):
