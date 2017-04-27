c7318a8bfec4e68ad7ef01c60c16b4a6e2cb765d
smx2020 <smx2020@gmail.com>
2017-04-27 15:01:10 +0200

    ajout

--- TP12/Fonction.py
+++ TP12/Fonction.py
@@ -1,7 +1,7 @@
 import os
 
 def Taille(name):
-	f = open(name)
+	f = open(name,'r')
 	L = f.readlines()
 	S = 0
 	for k in range(len(L)):
@@ -9,3 +9,16 @@ def Taille(name):
 			if L[k][i] != ' ' and L[k][i] != '\n' :
 				S = S + 1
 	return(k+1,S)
+
+def un_sur_deux(name1,name2):
+	f1=open(name1,'r')
+	f2=open(name2,'w')
+	L = f1.readlines()
+	for i in range(0,len(L),2):
+		f2.write(L[i])
+	f1.close()
+	f2.close()
+
+def liste_mots(name):
+	f=open(name,'r')
+	L = []
