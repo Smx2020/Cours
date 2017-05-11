ff08788843cd8c29d4aa07c176c2d5305eae1b1f
smx2020 <smx2020@gmail.com>
2017-05-11 14:56:53 +0200

    ajout de exercice 6

--- TP12/Exercice.py
+++ TP12/Exercice.py
@@ -4,23 +4,40 @@ from Fonction import *
 #trace_mouvement('TP13-Mouvement-Eleves.csv')
 #trace_vitesse('TP13-Vitesses-Eleves.csv')
 
-F1 = 'TP13-valeurs-Eleves.csv'
-f1=open(F1,"r")   # on ouvre le fichier
+#F1 = 'TP13-valeurs-Eleves.csv'
+#f1=open(F1,"r")   # on ouvre le fichier
 #i = 0
 #for value in csv.reader(f1):
 #	i = i + 1
 #print(i)
 
-i = 0
-Lx = []
-Ly = []
-for line in csv.reader(f1):
-	Lx.append(float(line[0]))
-	Ly.append(float(line[1]))
+#i = 0
+#Lx = []
+#Ly = []
+#for line in csv.reader(f1):
+#	Lx.append(float(line[0]))
+#	Ly.append(float(line[1]))
 #print(Lx,Ly)
 
 #plt.plot(Lx,Ly,'k*',label='Y en fonction de X')
 #plt.show()
 
-print(trapezes(Lx,Ly))
-print(trapz(Lx,Ly))
+#print(trapezes(Lx,Ly))
+#print(trapz(Lx,Ly))
+
+F5 = 'TP13-acquisition-Eleves.csv'
+f5=open(F5,"r")   # on ouvre le fichier
+i = 0
+D = []
+T = []
+TT = []
+for line in csv.reader(f5):
+	if i :
+		D.append(float(line[1]))
+		T.append(float(line[0]))
+		TT.append(float(line[0])*float(line[0]))
+	i = i + 1
+print(D)
+plt.plot(T,D,'k*',label='D en fonction de T')
+plt.plot(TT,D,'k*',label='D en fonction de T*T')
+plt.show()
